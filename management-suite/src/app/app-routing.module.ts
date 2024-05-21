5ed4740c38fdad816369b3cb2c7a88d7d6e3e40d
Rajendra Lad <ladrajendra7@gmail.com>
2024-05-21 18:51:46 +0530

    signin & signup basic page added

--- management-suite/src/app/app-routing.module.ts
+++ management-suite/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './account/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: LandingPageComponent, path: '' },
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.module').then((m) => m.AccountModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
